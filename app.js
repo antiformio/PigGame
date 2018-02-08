62baa39526b4f43774bea777877f377668cb3258
Filipe Martins <1120170@isep.ipp.pt>
2018-02-08 19:19:37 +0000

    Final commit.

--- app.js
+++ app.js
@@ -1,196 +1,183 @@
     /*
-    GAME RULES:
+            GAME RULES:
 
-    - The game has 2 players, playing in rounds
-    - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-    - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-    - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-    - The first player to reach 100 points on GLOBAL score wins the game
+            - The game has 2 players, playing in rounds
+            - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+            - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+            - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+            - The first player to reach 100 points on GLOBAL score wins the game
 
-    */
+            */
 
     var scores, roundScore, activePlayer, gamePlaying, maxScore;
 
     init();
 
 
-    document.querySelector('.btn-roll').addEventListener('click', function() {
-            // Se o jogo estiver no estado de jogar (true) então segue
-            if(gamePlaying){
-               
-                // Random number 1-6
-                var dice = Math.floor(Math.random() * 6) + 1;
-                var dice2 = Math.floor(Math.random() * 6) + 1;
+    document.querySelector('.btn-roll').addEventListener('click', function () {
+        // Se o jogo estiver no estado de jogar (true) então segue
+        if (gamePlaying) {
 
-                // Display the result
-                var diceDOM = document.querySelector('.dice');
-                var diceDOM2 = document.querySelector('.dice2'); 
+            // Random number 1-6
+            var dice = Math.floor(Math.random() * 6) + 1;
+            var dice2 = Math.floor(Math.random() * 6) + 1;
 
-
-
-                // Gif de rodar os dados/explosao
-                show();
-                function show() {
-                    if(dice == 1){
-                        diceDOM.src = 'explosao.gif';
-                    }else if(dice2 == 1){
-                        diceDOM2.src = 'explosao.gif';
-                        
-                    }else{
-                        diceDOM.src = 'giphy.gif';
-                        diceDOM2.src = 'giphy2.gif';
-                    }
+            // Display the result
+            var diceDOM = document.querySelector('.dice');
+            var diceDOM2 = document.querySelector('.dice2');
 
 
-                diceDOM.style.display='block';
-                diceDOM2.style.display='block';
+
+            // Gif de rodar os dados/explosao
+            show();
+
+            function show() {
+                if (dice == 1) {
+                    diceDOM.src = 'explosao.gif';
+                } else if (dice2 == 1) {
+                    diceDOM2.src = 'explosao.gif';
+
+                } else {
+                    diceDOM.src = 'giphy.gif';
+                    diceDOM2.src = 'giphy2.gif';
+                }
+
+
+                diceDOM.style.display = 'block';
+                diceDOM2.style.display = 'block';
 
                 // Quando passar 1 segundo chama a função 'hide'
-                setTimeout(hide, 1000);  // 3 seconds
-                }
-
-                function hide() {
-
-                    diceDOM.src = 'dice-'+ dice + '.png';
-                    diceDOM2.src = 'dice-'+ dice2 + '.png';
-                    // Update ao roundScore só se o numero que saiu nao é 1...
-                    // !== não faz type cohersion, podem ser de tipos diferentes
-                    if(dice !== 1 && dice2 !== 1){
-                        roundScore += dice + dice2;
-                        document.getElementById('current-' + activePlayer).textContent =roundScore;
-
-                    }else{
-                        // RoundScore passa a zero , porque lhe saíu 1 nos dados
-                        roundScore = 0;
-                        // Display de 0 no Ronda do activePlayer
-                        document.getElementById('current-' + activePlayer).textContent = '0';
-
-
-                        nextPlayer();
-
-
-
-                    }
-                }
-
+                setTimeout(hide, 1000); // 3 seconds
             }
+
+            function hide() {
+
+                diceDOM.src = 'dice-' + dice + '.png';
+                diceDOM2.src = 'dice-' + dice2 + '.png';
+                // Update ao roundScore só se o numero que saiu nao é 1...
+                // !== não faz type cohersion, podem ser de tipos diferentes
+                if (dice !== 1 && dice2 !== 1) {
+                    roundScore += dice + dice2;
+                    document.getElementById('current-' + activePlayer).textContent = roundScore;
+
+                } else {
+                    // RoundScore passa a zero , porque lhe saíu 1 nos dados
+                    roundScore = 0;
+                    // Display de 0 no Ronda do activePlayer
+                    document.getElementById('current-' + activePlayer).textContent = '0';
+
+
+                    nextPlayer();
+
+
+
+                }
+            }
+
+        }
     });
 
-        document.querySelector('.btn-hold').addEventListener('click', function(){
-                
-            if(gamePlaying){
-                scores[activePlayer] += roundScore;
-                var input = document.querySelector('.final-score').value;
-                var winningScore;
-                // Se for undefined, 0, null ou "", então dá falso
-                // se não for, da true.
-                if(input){
-                     winningScore = input;
-                }else{
-                    // Se não for definido fica 100;
-                    winningScore = 100;
-                }
-                if(scores[activePlayer] >= input){
-                    document.getElementById('score-'+activePlayer).textContent = scores[activePlayer];
-                    document.getElementById('name-'+activePlayer).textContent = 'Ganhou !'
-                    // Esconde dado
-                    document.querySelector('.dice').style.display = 'none';
-                    document.querySelector('.dice2').style.display = 'none';
-                    // Aplica a classe winner do css, e remove a classe active 
-                    document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
-                    document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
-                    // Mudar o estado do jogo para falso
-                    gamePlaying = false;
-                    
-                    
-                }else{
-                    document.getElementById('score-'+activePlayer).textContent = scores[activePlayer];
-                    nextPlayer();
-                }
-                
+    document.querySelector('.btn-hold').addEventListener('click', function () {
+
+        if (gamePlaying) {
+            scores[activePlayer] += roundScore;
+            var input = document.querySelector('.final-score').value;
+            var winningScore;
+            // Se for undefined, 0, null ou "", então dá falso
+            // se não for, da true.
+            if (input) {
+                winningScore = input;
+            } else {
+                // Se não for definido fica 100;
+                winningScore = 100;
+            }
+            if (scores[activePlayer] >= input) {
+                document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+                document.getElementById('name-' + activePlayer).textContent = 'Ganhou !'
+                // Esconde dado
+                document.querySelector('.dice').style.display = 'none';
+                document.querySelector('.dice2').style.display = 'none';
+                // Aplica a classe winner do css, e remove a classe active 
+                document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+                document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+                // Mudar o estado do jogo para falso
+                gamePlaying = false;
+
+
+            } else {
+                document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+                nextPlayer();
             }
 
-
-
-        });
-
-        function nextPlayer(){
-
-            // Mudar o activePlayer
-                        
-
-                        // Mudar o css do activePlayer
-                        //      Toggle remove ou adiciona a palavra active à classe actual. Se ele está active, remove, se não está adiciona.
-                        document.querySelector('.player-0-panel').classList.toggle('active');
-                        document.querySelector('.player-1-panel').classList.toggle('active');
-
-                        // Se não quisesse fazer toggle, podia usar os métodos add e remove da classList:
-                        // document.querySelector('.player-1-panel).classList.remove('active);
-                        // document.querySelector('.player-1-panel).classList.add('active);
-
-                        // Esconder o dado no final
-                        document.querySelector('.dice').style.display = 'none';
-                        document.querySelector('.dice2').style.display = 'none';
-                        roundScore= 0;
-                        document.getElementById('current-'+activePlayer).textContent = 0;
-                        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-
-
-        };
-
-        function init(){
-            
-            // Reset das variaveis de jogo
-            scores = [0,0];
-            roundScore = 0;
-            activePlayer = 0; // 0 -> player1 1 -> player2
-            gamePlaying = true;
-            
-            
-            // Reset do display das variaveis
-            document.getElementById('score-0').textContent = '0';
-            document.getElementById('score-1').textContent = '0';
-            document.getElementById('current-0').textContent = '0';
-            document.getElementById('current-1').textContent = '0';
-            
-            // Set players name
-            document.getElementById('name-0').textContent = 'Player 1';
-            document.getElementById('name-1').textContent = 'Player 2';
-            
-            
-
-            // Remover classes de winners e active players
-            document.querySelector('.player-0-panel').classList.remove('winner');
-            document.querySelector('.player-1-panel').classList.remove('winner');
-            document.querySelector('.player-0-panel').classList.add('active'); // Quando começa o active player é o 1...
-            document.querySelector('.player-1-panel').classList.remove('active');
-
-            // Se quisesse acrescentar codigo html...
-                // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-            // Ler do DOM
-                //var x = document.querySelector('#score-0').textContent;
-
-            // Mudar o CSS de um elemento
-                // Esconder uma imagem
-                // O querySelector aqui vai buscar a classe da imagem, ou seja '.dice' depois aceder à propriedade display e mudar para none
-            document.querySelector('.dice').style.display = 'none';
-            document.querySelector('.dice2').style.display = 'none';
-            
-        };
-
-        document.querySelector('.btn-new').addEventListener('click', init);
+        }
 
 
 
+    });
+
+    function nextPlayer() {
+
+        // Mudar o activePlayer
+
+
+        // Mudar o css do activePlayer
+        //      Toggle remove ou adiciona a palavra active à classe actual. Se ele está active, remove, se não está adiciona.
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        // Se não quisesse fazer toggle, podia usar os métodos add e remove da classList:
+        // document.querySelector('.player-1-panel).classList.remove('active);
+        // document.querySelector('.player-1-panel).classList.add('active);
+
+        // Esconder o dado no final
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.dice2').style.display = 'none';
+        roundScore = 0;
+        document.getElementById('current-' + activePlayer).textContent = 0;
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+
+    };
+
+    function init() {
+
+        // Reset das variaveis de jogo
+        scores = [0, 0];
+        roundScore = 0;
+        activePlayer = 0; // 0 -> player1 1 -> player2
+        gamePlaying = true;
+
+
+        // Reset do display das variaveis
+        document.getElementById('score-0').textContent = '0';
+        document.getElementById('score-1').textContent = '0';
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        // Set players name
+        document.getElementById('name-0').textContent = 'Player 1';
+        document.getElementById('name-1').textContent = 'Player 2';
 
 
 
+        // Remover classes de winners e active players
+        document.querySelector('.player-0-panel').classList.remove('winner');
+        document.querySelector('.player-1-panel').classList.remove('winner');
+        document.querySelector('.player-0-panel').classList.add('active'); // Quando começa o active player é o 1...
+        document.querySelector('.player-1-panel').classList.remove('active');
 
+        // Se quisesse acrescentar codigo html...
+        // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
+        // Ler do DOM
+        //var x = document.querySelector('#score-0').textContent;
 
+        // Mudar o CSS de um elemento
+        // Esconder uma imagem
+        // O querySelector aqui vai buscar a classe da imagem, ou seja '.dice' depois aceder à propriedade display e mudar para none
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.dice2').style.display = 'none';
 
+    };
 
-
-
-
+    document.querySelector('.btn-new').addEventListener('click', init);
